b15b66e69fd61ff92a282f559c2322547379ef47
Aisha Osman <aisha.osman09@gmail.com>
2022-01-06 20:32:25 -0600

    updates

---
+++ public/js/idb.js
@@ -0,0 +1,60 @@
+let db;
+
+//
+
+const request = indexedDB.open('budget-tracker', 1);
+
+//
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
+
+//on success
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+    if (navigator.onLine) {
+        uploadTransaction();
+    }
+};
+
+//on error
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
+
+// save
+
+function saveRecord(record) {
+    const transaction = db.transaction(["new_budget"], "readwrite");
+    const store = transaction.objectStore("new_budget");
+
+    //
+
+    const getAll = store.getAll();
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            }).then((response) => response.json())
+            .then(() => {
+                const transaction = db.transaction(["new_budget"], "readwrite");
+                const store = transaction.objectStore("new_budget");
+                store.clear();
+            }).catch(err => {
+                console.log(err);
+            });
+        }
+    };
+    
+}
+
+window.addEventListener("online", addTransaction);
